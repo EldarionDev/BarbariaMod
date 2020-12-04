8787a247c813256249eb98769bb369c1ae485c39
EldarionDev <66467603+EldarionDev@users.noreply.github.com>
2020-12-04 17:52:41 +0100

    Removed unused includes, class for mod constants.

--- src/main/java/barbaria/barbaria/Barbaria.java
+++ src/main/java/barbaria/barbaria/Barbaria.java
@@ -1,36 +1,27 @@
 package barbaria.barbaria;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
-        modid = Barbaria.MOD_ID,
-        name = Barbaria.MOD_NAME,
-        version = Barbaria.VERSION
+        modid = BarbariaConstants.MOD_ID,
+        name = BarbariaConstants.MOD_NAME,
+        version = BarbariaConstants.VERSION
 )
 public class Barbaria {
-
-    public static final String MOD_ID = "barbaria";
-    public static final String MOD_NAME = "Barbaria";
-    public static final String VERSION = "1.0-SNAPSHOT";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
      */
-    @Mod.Instance(MOD_ID)
+    @Mod.Instance(BarbariaConstants.MOD_ID)
     public static Barbaria INSTANCE;
 
-    public static final Logger LOGGER = LogManager.getLogger(Barbaria.MOD_ID);
+    public static final Logger LOGGER = LogManager.getLogger(BarbariaConstants.MOD_ID);
 
     /**
      * This is the first initialization event. Register tile entities here.
---
+++ src/main/java/barbaria/barbaria/BarbariaConstants.java
@@ -0,0 +1,7 @@
+package barbaria.barbaria;
+
+public class BarbariaConstants {
+    public static final String MOD_ID = "barbaria";
+    public static final String MOD_NAME = "Barbaria";
+    public static final String VERSION = "1.0-SNAPSHOT";
+}
