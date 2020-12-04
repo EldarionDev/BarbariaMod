3710c6cc910eec82ef519baf6d274b019ec0c91c
EldarionDev <66467603+EldarionDev@users.noreply.github.com>
2020-12-04 19:55:32 +0100

    Added proxy system.

--- src/main/java/barbaria/barbaria/Barbaria.java
+++ src/main/java/barbaria/barbaria/Barbaria.java
@@ -1,5 +1,7 @@
 package barbaria.barbaria;
 
+import barbaria.barbaria.proxy.CommonProxy;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -19,7 +21,6 @@ public class Barbaria {
      * This is the instance of your mod as created by Forge. It will never be null.
      */
     @Mod.Instance(BarbariaConstants.MOD_ID)
-    public static Barbaria INSTANCE;
 
     public static final Logger LOGGER = LogManager.getLogger(BarbariaConstants.MOD_ID);
 
@@ -30,6 +31,8 @@ public class Barbaria {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Pre-initializing Barbaria Mod.");
+        BarbariaConstants.setModInstance(this);
+        proxy.preInit(event);
     }
 
     /**
@@ -38,6 +41,7 @@ public class Barbaria {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         LOGGER.info("Initializing Barbaria Mod.");
+        proxy.init(event);
     }
 
     /**
@@ -46,5 +50,9 @@ public class Barbaria {
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         LOGGER.info("Loaded Barbaria Mod.");
+        proxy.postInit(event);
     }
+
+    @SidedProxy(serverSide = "barbaria.barbaria.proxy.ServerProxy", clientSide = "barbaria.barbaria.proxy.ClientProxy")
+    private static CommonProxy proxy;
 }
--- src/main/java/barbaria/barbaria/BarbariaConstants.java
+++ src/main/java/barbaria/barbaria/BarbariaConstants.java
@@ -4,4 +4,14 @@ public class BarbariaConstants {
     public static final String MOD_ID = "barbaria";
     public static final String MOD_NAME = "Barbaria";
     public static final String VERSION = "1.0-SNAPSHOT";
+
+    private static Barbaria INSTANCE;
+
+    public static Barbaria getModInstance () {
+        return INSTANCE;
+    }
+
+    public static void setModInstance (Barbaria instance) {
+        INSTANCE = instance;
+    }
 }
